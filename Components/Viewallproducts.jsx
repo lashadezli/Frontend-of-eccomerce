da9f63e8ae18a6a4e1bc1fed929817eea096143b
Lasha Giorgi Qutateladze <lashagiorgi07@gmail.com>
2024-05-26 14:39:31 +0400

    fixed link

--- Components/Viewallproducts.jsx
+++ Components/Viewallproducts.jsx
@@ -1,10 +1,10 @@
-import classes from  '../modules/View.module.scss'
+import classes from "../modules/View.module.scss";
+import { Link } from "react-router-dom";
 
 const ViewAll = () => {
   return (
-
-    <div className={classes['View-all']}>   
-       <button>View All Products</button>
+    <div className={classes["View-all"]}>
+      <Link to="/">View All Product</Link>
     </div>
   );
 };
